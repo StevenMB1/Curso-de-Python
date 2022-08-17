36d1ecb10a426016fe85141c112c7bbb21743eda
Steven Medina <74547441+StevenMB1@users.noreply.github.com>
2022-08-17 11:16:30 -0400

    Actualizacion

--- 04-ejercicios/calculadora_suma.py
+++ 04-ejercicios/calculadora_suma.py
@@ -1,20 +1,87 @@
-primero = input('Ingrese un numero: ')
+#TODO Calculadora que suma.
+
+# primero = input('Ingrese primer numero: ')
+
+# try:
+#     primero = int(primero)
+# except:
+#     primero = ''
+    
+
+# segundo = input('Ingrese segundo numero: ')
+
+# try:
+#     segundo = int(segundo)
+# except:
+#     segundo = ''
+    
+# if primero == '' or segundo == '':
+#     print('Ingresaste mal un dato, prueba de nuevo solo con numeros')
+# else:
+#     print(primero + segundo)
+
+
+#TODO Moviendo la validacion
+
+# primero = input('Ingrese primer numero: ')
+
+# try:
+#     primero = int(primero)
+# except:
+#     primero = ''
+    
+# if primero == '':
+#     print('El valor ingresado no es un entero')
+#     exit()
+
+# segundo = input('Ingrese segundo numero: ')
+
+# try:
+#     segundo = int(segundo)
+# except:
+#     segundo = ''
+    
+# if segundo == '':
+#     print('El valor ingresado no es un entero')
+#     exit()
+
+# print(primero + segundo)
+
+
+#TODO Agregando mas operaciones
+
+primero = input('Ingrese primer numero: ')
 
 try:
     primero = int(primero)
 except:
-    primero = 'chanchito feliz'
+    primero = ''
+    
+if primero == '':
+    print('El valor ingresado no es un entero')
+    exit()
 
-segundo = input('Ingrese un segundo numero: ')
+segundo = input('Ingrese segundo numero: ')
 
 try:
     segundo = int(segundo)
 except:
-    segundo = 'chanchito feliz'
+    segundo = ''
     
-if primero == 'chanchito feliz' or segundo == 'chanchito feliz':
-    print('Ingresaste mal un dato, prueba de nuevo solo con numeros')
+if segundo == '':
+    print('El valor ingresado no es un entero')
+    exit()
+
+simbolo = input('Ingrese operacion: ')
+
+if simbolo == '+':
+    print('Suma:', primero + segundo)
+elif simbolo == '-':
+    print('Resta:', primero - segundo)
+elif simbolo == '*':
+    print('Multiplicacion', primero * segundo)
+elif simbolo == '/':
+    print('Division', primero / segundo)
 else:
-    print(primero + segundo)
-
-
+    print('El simbolo ingresado no es valido')
+    
